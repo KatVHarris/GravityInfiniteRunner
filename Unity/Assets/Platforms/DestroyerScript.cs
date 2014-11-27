893ac4545d7860212ee68eec0d28bd7d02363562
Kat Harris <kvm.harris@gmail.com>
2014-11-27 01:47:44 -0800

    TestScene Spawn and controller
    Has the code for setting up the script to generate path/noPath levels.
    It also has the control for flipping an object on command, aka what will
    be used on the Character Controller.

---
+++ Unity/Assets/Platforms/DestroyerScript.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DestroyerScript : MonoBehaviour
+{
+
+	void OnTriggerEnter (Collider other)
+	{
+		if (other.tag == "Player") {
+			Debug.Break ();
+			return;
+		}
+
+		//Destory object I encounter, and it's parents
+		if (other.gameObject.transform.parent) {
+			Destroy (other.gameObject.transform.parent.gameObject);
+		} else {
+			Destroy (other.gameObject);
+		}
+	}
+}
