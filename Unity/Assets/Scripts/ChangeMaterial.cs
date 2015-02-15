f03aa652cf897f1ba9160265df725079ddb43ff0
KatVHarris <kvm.harris@gmail.com>
2015-02-15 13:27:02 -0800

    Adjusted shooting

---
+++ Unity/Assets/Scripts/ChangeMaterial.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ChangeMaterial : MonoBehaviour {
+
+	public Material[] materials;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public void Restore(){
+		renderer.sharedMaterial = materials[0];
+	}
+
+	public void ChangeColor(){
+		renderer.sharedMaterial = materials[1];
+	}
+}
