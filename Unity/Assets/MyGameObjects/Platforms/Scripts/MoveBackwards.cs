bb19bd8bc67d19ef5de4035fb1eb9c3ef51f9621
KatVHarris <kvm.harris@gmail.com>
2015-02-20 14:43:15 -0800

    Backwards spawning

---
+++ Unity/Assets/MyGameObjects/Platforms/Scripts/MoveBackwards.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MoveBackwards : MonoBehaviour {
+	public float backspeed = 3.0f;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.Translate(Vector3.back * Time.deltaTime * backspeed);
+	}
+}
